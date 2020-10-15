71980a2ec70fd86237d93b507d60f6b5a4461262
C <julien.corvaisier.br@gmail.com>
2020-10-15 10:11:43 +0200

    qcm

--- script.js
+++ script.js
@@ -15,18 +15,27 @@ const populations = [
 ];
 
 // tidy array by length
-populations.sort(function(a,b){ return a.name.length - b.name.length})
+populations.sort(function (a, b) { return a.name.length - b.name.length })
 
 // pin up lenName
-const newPopulation = populations.map( population => ({...population, lenName: population.name.length }))
+const newPopulation = populations.map(population => ({ ...population, lenName: population.name.length }))
 
 //console.log(newPopulation)
 
 //tidy and push in specific array
 let groupNames = newPopulation.reduce((b, a) => {
-b[a.lenName] = [...b[a.lenName] || [] , a];
-return b;
+  b[a.lenName] = [...b[a.lenName] || [], a];
+  return b;
 }, {});
-console.log(groupNames)
+//console.log(groupNames)
 
+const relations = [
+  { id: 0, relation: [1, 2, 4] },
+  { id: 3, relation: [7, 8] },
+  { id: 4, relation: [2, 7, 8, 11] },
+  { id: 5, relation: [1, 2, 4] },
+  { id: 7, relation: [2, 3, 5, 9] },
+  { id: 9, relation: [1, 2, 4, 8, 11] },
+  { id: 11, relation: [1, 2, 9, 10,] },
+]
 
---
+++ test_tp.js
@@ -0,0 +1,4 @@
+const t1 = [1, 2];
+const t2 = t1;
+t1.push(3);
+console.log(t1);
